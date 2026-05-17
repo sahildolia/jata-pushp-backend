2282c905d38f87fbc1be6a33ef8a256c222a108d
Sahil Dolia <sahildolia.456@gmail.com>
2026-05-17 15:11:33 +0530

    Code Update

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const UserModel = require("./models/Employee");
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(
-  "mongodb+srv://sahildolia456:3gLx72p9uRwUcHwj@cluster0.c99om.mongodb.net/"
-);
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 app.post("/login", (req, res) => {
   const { email, password } = req.body;
+
   UserModel.findOne({ email: email }).then((user) => {
     if (user) {
       if (user.password === password) {
